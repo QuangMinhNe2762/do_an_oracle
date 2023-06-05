60dc56f964628b7f9c1aa5c7fe292eb3a6314c75
JFN NAME_GIT <minhdoan2762002@gmail.com>
2023-06-05 16:39:36 +0700

    chuong_trinh_quanly_thucung

---
+++ chuong_trinh_quanly_thucung/DoAn_PetShop/DoAn_PetShop/ChangePassword.cs
@@ -0,0 +1,82 @@
+﻿using DoAn_PetShop.CSDL;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn_PetShop
+{
+    public partial class ChangePassword : Form
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangePassword_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndmk_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "" || txtPassword.Text == "")
+            {
+                MessageBox.Show("vui lòng điền đầy đủ thông tin");
+            }
+            else
+            {
+                DbConnect dbcon = new DbConnect();
+                OracleConnection con = dbcon.connectionSYS();
+                if (con != null)
+                {
+                    try
+                    {
+                        OracleCommand cmd = new OracleCommand("alter user " + txtUser.Text + " identified by " + txtPassword.Text + "", con);
+                        try
+                        {
+                            con.Open();
+                        }
+                        catch
+                        {
+                            con.Close();
+                            MessageBox.Show("đổi mật khẩu thất bại \ncần đăng nhập vào chương trình \nbằng sys sau đó quay lại đổi mật khẩu user mong muốn");
+                            return;
+                        }
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("đổi mật khẩu thành công");
+                            con.Close();
+                            this.Close();
+                    }
+                    catch
+                    {
+                        con.Close();
+                        MessageBox.Show("không được đổi mật khẩu đã được sử dụng");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("vì lý do bắt buộc cần\nđăng nhập chương trình bằng user sys sau đó\nquay lại đổi mật khẩu user mong muốn");
+                }
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPassword.TextLength > 2)
+            {
+                btndmk.Enabled = true;
+            }
+            else
+            {
+                btndmk.Enabled = false;
+            }
+        }
+    }
+}
---
+++ chuong_trinh_quanly_thucung/DoAn_PetShop/DoAn_PetShop/ChangePassword.designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace DoAn_PetShop
+{
+    partial class ChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btndmk = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(37)))), ((int)(((byte)(83)))));
+            this.label3.Location = new System.Drawing.Point(29, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "User";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUser.DefaultText = "";
+            this.txtUser.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtUser.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtUser.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUser.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUser.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUser.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtUser.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUser.Location = new System.Drawing.Point(32, 71);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.PasswordChar = '\0';
+            this.txtUser.PlaceholderText = "Nhập tên user";
+            this.txtUser.SelectedText = "";
+            this.txtUser.Size = new System.Drawing.Size(200, 36);
+            this.txtUser.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(37)))), ((int)(((byte)(83)))));
+            this.label1.Location = new System.Drawing.Point(29, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderRadius = 5;
+            this.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPassword.DefaultText = "";
+            this.txtPassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtPassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtPassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPassword.Font = new System.Drawing.Font("Century Gothic", 9F);
+            this.txtPassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPassword.IconRightOffset = new System.Drawing.Point(2, 0);
+            this.txtPassword.Location = new System.Drawing.Point(32, 165);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '•';
+            this.txtPassword.PlaceholderText = "Mật khẩu mới";
+            this.txtPassword.SelectedText = "";
+            this.txtPassword.Size = new System.Drawing.Size(200, 36);
+            this.txtPassword.TabIndex = 59;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // btndmk
+            // 
+            this.btndmk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btndmk.BackColor = System.Drawing.SystemColors.Control;
+            this.btndmk.BorderRadius = 10;
+            this.btndmk.BorderThickness = 2;
+            this.btndmk.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btndmk.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btndmk.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btndmk.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btndmk.Enabled = false;
+            this.btndmk.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(98)))), ((int)(((byte)(19)))));
+            this.btndmk.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btndmk.ForeColor = System.Drawing.Color.Black;
+            this.btndmk.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.btndmk.Location = new System.Drawing.Point(32, 234);
+            this.btndmk.Name = "btndmk";
+            this.btndmk.Size = new System.Drawing.Size(195, 34);
+            this.btndmk.TabIndex = 60;
+            this.btndmk.Text = "Đổi mật khẩu";
+            this.btndmk.Click += new System.EventHandler(this.btndmk_Click);
+            // 
+            // ChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 307);
+            this.Controls.Add(this.btndmk);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Name = "ChangePassword";
+            this.Text = "ChangePassword";
+            this.Load += new System.EventHandler(this.ChangePassword_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        public Guna.UI2.WinForms.Guna2TextBox txtPassword;
+        private Guna.UI2.WinForms.Guna2Button btndmk;
+    }
+}
